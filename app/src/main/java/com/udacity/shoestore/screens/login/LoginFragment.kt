273a3d8e73c141cdef5b171fe0c8cf73386c0492
JaviMera <javier_mera88@hotmail.com>
2021-07-31 17:48:59 -0500

    Added Login destination and set it as start up destination.

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,9 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        binding =DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        return binding.root
     }
 }
