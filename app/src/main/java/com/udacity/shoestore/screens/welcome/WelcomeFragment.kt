f46beeac456a50a6da52ae64d707f08d84e726ee
JaviMera <javier_mera88@hotmail.com>
2021-08-01 14:59:23 -0500

    Added instructions fragment and added it as a destination.

--- app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -23,6 +24,10 @@ class WelcomeFragment : Fragment() {
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        binding.buttonContinue.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeDestinationToInstructionsFragment())
+        }
 
         return binding.root
     }
